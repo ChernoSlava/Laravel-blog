78a925659efe5fb94c1691178f1f4869957cc88c
ChernoSlava <slavachernogorv@mail.ru>
2024-01-19 14:44:34 +0200

    feat: add directives, shared and layouts.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,23 +1,14 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel | About</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
-        <a href="/">back</a>
-        <div class="container mt-4">
-            <h1>О блоге</h1>
-            <p>Эксперименты с Ларавелем на Хекслете</p>
-            <div style="display: flex;gap:15px;">
-                @foreach ($tags as $tag)
-                    <p>{{ $tag }}</p>
-                @endforeach
-            </div>
-            <!-- <p>{{ implode(', ', $tags) }}</p> -->
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('header', 'О блоге')
+
+@section('content')
+    <p>Эксперименты с Laravel на Хекслете</p>
+    <div style="display: flex;gap:15px;">
+        @foreach ($tags as $tag)
+            <p>{{ $tag }}</p>
+        @endforeach
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,28 +1,20 @@
-<!-- <!doctype html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel | Articles</title>
+        <title>Hexlet Blog - @yield('title')</title>
+        @include('shared.metatags')
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
-        <a href="/">back</a>
         <div class="container mt-4">
-            <h1>Статьи</h1>
+            <a href="/">back</a>
+            <h1>@yield('header')</h1>
+            <div>
+                @yield('content')
+            </div>
         </div>
     </body>
-</html> -->
-
-@extends('layouts.app')
-
-@section('title', 'Статьи')
-
-@section('header', 'Статьи')
-
-@section('content')
-    <div class="container mt-4">
-        <p>Страница со статьями</х>
-    </div>
-@endsection
+</html>
