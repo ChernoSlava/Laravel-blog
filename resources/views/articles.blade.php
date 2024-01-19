1bd173a97b44e7b08244da88464cb8af4b734978
ChernoSlava <slavachernogorv@mail.ru>
2024-01-19 12:17:04 +0200

    feat: add new route and page article, add links.

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -3,15 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel | About</title>
+        <title>Laravel | Articles</title>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
         <a href="/">back</a>
         <div class="container mt-4">
-            <h1>О блоге</h1>
-            <p>Эксперименты с Ларавелем на Хекслете</p>
+            <h1>Статьи</h1>
         </div>
     </body>
 </html>
